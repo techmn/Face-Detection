108b9afa304e43b44100f3c9f0297caa451c0302
TechMN <techmn@users.noreply.github.com>
2018-03-23 00:08:50 +0500

    Add files via upload

---
+++ FaceDetection/FaceDetection/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.startCamBtn = new System.Windows.Forms.Button();
+            this.stopCamBtn = new System.Windows.Forms.Button();
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.stopCamBtn);
+            this.groupBox1.Controls.Add(this.startCamBtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 318);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.imgBox);
+            this.groupBox2.Location = new System.Drawing.Point(212, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 318);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // startCamBtn
+            // 
+            this.startCamBtn.Location = new System.Drawing.Point(37, 38);
+            this.startCamBtn.Name = "startCamBtn";
+            this.startCamBtn.Size = new System.Drawing.Size(119, 23);
+            this.startCamBtn.TabIndex = 0;
+            this.startCamBtn.Text = "Start Camera";
+            this.startCamBtn.UseVisualStyleBackColor = true;
+            this.startCamBtn.Click += new System.EventHandler(this.startCamBtn_Click);
+            // 
+            // stopCamBtn
+            // 
+            this.stopCamBtn.Location = new System.Drawing.Point(37, 67);
+            this.stopCamBtn.Name = "stopCamBtn";
+            this.stopCamBtn.Size = new System.Drawing.Size(119, 23);
+            this.stopCamBtn.TabIndex = 1;
+            this.stopCamBtn.Text = "Stop Camera";
+            this.stopCamBtn.UseVisualStyleBackColor = true;
+            this.stopCamBtn.Click += new System.EventHandler(this.stopCamBtn_Click);
+            // 
+            // imgBox
+            // 
+            this.imgBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.imgBox.Location = new System.Drawing.Point(40, 41);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(320, 240);
+            this.imgBox.TabIndex = 0;
+            this.imgBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 342);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(640, 380);
+            this.MinimumSize = new System.Drawing.Size(640, 380);
+            this.Name = "Form1";
+            this.Text = "Face Detection";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button stopCamBtn;
+        private System.Windows.Forms.Button startCamBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox imgBox;
+    }
+}
+
---
+++ FaceDetection/FaceDetection/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        delegate void ShowImageDelegate();
+
+        private VideoCapture capture;
+        private CascadeClassifier detector;
+        private Thread camThread;
+        private bool isCameraOn;
+        private Mat frame;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initData();
+        }
+
+        private void initData()
+        {
+            try
+            {
+                isCameraOn = false;
+
+                // Open default/first camera
+                capture = new VideoCapture(0);
+                detector = new CascadeClassifier("../../haarcascade_frontalface_default.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void showImage()
+        {
+            if (imgBox.InvokeRequired)
+            {
+                // instantiate delegate to access picture box in thread
+                ShowImageDelegate sid = new ShowImageDelegate(showImage);
+                this.Invoke(sid);
+            }
+            else
+            {
+                imgBox.BackgroundImage = frame.Bitmap;  // update BackgroundImage of picturebox
+            }
+        }
+
+        private void detectFaces()
+        {
+            while (isCameraOn)
+            {
+                try
+                {
+                    frame = capture.QueryFrame();                           // capture image
+                    Rectangle[] faces = detector.DetectMultiScale(frame);   // detect faces in the frame
+
+                    for (int i = 0; i < faces.Length; i++)
+                    {
+                        Rectangle r = faces[i];
+                        CvInvoke.Rectangle(frame, r, new MCvScalar(0, 0, 255), 2);
+                    }
+                    showImage();                        // show captured image
+                    Thread.Sleep(40);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+            }
+        }
+
+        private void startCamBtn_Click(object sender, EventArgs e)
+        {
+            isCameraOn = true;
+            camThread = new Thread(detectFaces);  // create thread
+            camThread.Start();                  // start thread
+        }
+
+        private void stopCamBtn_Click(object sender, EventArgs e)
+        {
+            isCameraOn = false;
+        }
+    }
+}
